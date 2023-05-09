229395394204eadffa62a28d8b2f10c8430e6327
aoirint <aoirint@gmail.com>
2023-05-09 10:46:22 +0900

    merge selenium_playlist

---
+++ amzmusicplaylistdumper/api/selenium_playlist.py
@@ -0,0 +1,106 @@
+from pathlib import Path
+from typing import List
+from urllib.parse import urlparse
+from pydantic import BaseModel
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# from selenium.common.exceptions import NoSuchElementException
+import json
+from datetime import datetime, timezone
+import time
+from pathlib import Path
+import os
+
+from . import config
+from .amazon_url_utility import get_playlist_url
+
+
+class SeleniumPlaylistTrackItem(BaseModel):
+  index: str
+  title: str
+  url: str
+  artist: str
+  artist_url: str
+  album: str
+  album_url: str
+  duration: str
+  image_url: str
+
+
+class SeleniumPlaylist(BaseModel):
+  asin: str
+  tracks: list[SeleniumPlaylistTrackItem]
+
+
+def fetch_selenium_playlist(
+  selenium_url: str,
+  playlist_asin: str,
+) -> SeleniumPlaylist:
+  playlist_url = get_playlist_url(playlist_asin=playlist_asin)
+
+  options = webdriver.ChromeOptions()
+
+  driver = webdriver.Remote(
+    command_executor=selenium_url,
+    options=options,
+  )
+  driver.get(playlist_url)
+
+  time.sleep(1)
+
+  tracks: list[SeleniumPlaylistTrackItem] = []
+  last_index = 0
+
+  prev_scroll_y = -1
+  while True:
+    music_rows = driver.find_elements(by=By.CSS_SELECTOR, value='music-image-row')
+    for music_row in music_rows:
+      index = int(music_row.find_element(by=By.CSS_SELECTOR, value='.index').text)
+
+      image_elm = music_row.find_element(by=By.CSS_SELECTOR, value='music-image')
+      image_url = image_elm.get_attribute('src')
+
+      music_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col1 > music-link')
+      music_title = music_elm.get_attribute('title')
+      music_url = music_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
+
+      artist_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col2 > music-link')
+      artist = artist_elm.get_attribute('title')
+      artist_url = artist_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
+
+      album_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col3 > music-link')
+      album = album_elm.get_attribute('title')
+      album_url = album_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
+
+      duration_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col4 > music-link')
+      duration = duration_elm.get_attribute('title')
+
+      if last_index < index:
+        tracks.append(SeleniumPlaylistTrackItem(
+          index=index,
+          title=music_title,
+          url=music_url,
+          artist=artist,
+          artist_url=artist_url,
+          album=album,
+          album_url=album_url,
+          duration=duration,
+          image_url=image_url,
+        ))
+        last_index = index
+
+    driver.execute_script('window.scrollBy(0, 500)')
+    time.sleep(1)
+
+    scroll_y = driver.execute_script('return window.scrollY')
+    if prev_scroll_y == scroll_y:
+      break
+
+    prev_scroll_y = scroll_y
+
+  driver.quit()
+
+  return SeleniumPlaylist(
+    asin=playlist_asin,
+    tracks=tracks,
+  )
--- amzmusicplaylistdumper/main.py
+++ amzmusicplaylistdumper/main.py
@@ -1,86 +1,100 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-# from selenium.common.exceptions import NoSuchElementException
+import csv
 import json
+from io import StringIO
 from datetime import datetime, timezone
-import time
-from pathlib import Path
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
-PLAYLIST_URL = os.environ['PLAYLIST_URL']
-SELENIUM_URL = os.environ['SELENIUM_URL']
-ROOT_DIR = Path(os.environ['ROOT_DIR'])
-
-timestamp_utc_aware = datetime.now(timezone.utc)
-
-options = webdriver.ChromeOptions()
-
-driver = webdriver.Remote(
-    command_executor=SELENIUM_URL,
-    options=options,
-)
-driver.get(PLAYLIST_URL)
-
-time.sleep(1)
-
-musics = []
-last_index = 0
-
-prev_scroll_y = -1
-while True:
-  music_rows = driver.find_elements(by=By.CSS_SELECTOR, value='music-image-row')
-  for music_row in music_rows:
-    index = int(music_row.find_element(by=By.CSS_SELECTOR, value='.index').text)
-
-    image_elm = music_row.find_element(by=By.CSS_SELECTOR, value='music-image')
-    image_url = image_elm.get_attribute('src')
-
-    music_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col1 > music-link')
-    music_title = music_elm.get_attribute('title')
-    music_url = music_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
-
-    artist_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col2 > music-link')
-    artist = artist_elm.get_attribute('title')
-    artist_url = artist_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
-
-    album_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col3 > music-link')
-    album = album_elm.get_attribute('title')
-    album_url = album_elm.find_element(by=By.CSS_SELECTOR, value='a').get_attribute('href')
-
-    duration_elm = music_row.find_element(by=By.CSS_SELECTOR, value='.col4 > music-link')
-    duration = duration_elm.get_attribute('title')
-
-    if last_index < index:
-      musics.append({
-        'index': index,
-        'music_title': music_title,
-        'music_url': music_url,
-        'artist': artist,
-        'artist_url': artist_url,
-        'album': album,
-        'album_url': album_url,
-        'duration': duration,
-        'image_url': image_url,
-      })
-      last_index = index
-
-  driver.execute_script('window.scrollBy(0, 500)')
-  time.sleep(1)
-
-  scroll_y = driver.execute_script('return window.scrollY')
-  if prev_scroll_y == scroll_y:
-    break
-
-  prev_scroll_y = scroll_y
+from api.embed_playlist import fetch_embed_playlist
+from api.selenium_playlist import fetch_selenium_playlist
+from amzmusicplaylistdumper.api.amazon_url_utility import get_album_track_url, get_album_url, get_artist_url, get_playlist_track_url, get_playlist_url
 
 
-datetime_string = timestamp_utc_aware.strftime('%Y%m%d_%H%M%S')
-with open(ROOT_DIR / f'{datetime_string}.json', 'w', encoding='utf-8') as fp:
-  json.dump({
-    'musics': musics,
-    'fetched_at': timestamp_utc_aware.isoformat(),
-  }, fp, ensure_ascii=False)
+def command_embed_playlist_csv(args):
+  playlist_asin = args.playlist_asin
+  embed_playlist = fetch_embed_playlist(playlist_asin=playlist_asin)
 
-driver.quit()
+  sio = StringIO()
+  writer = csv.writer(sio)
+
+  writer.writerow([
+    'playlist_asin',
+    'playlist_url',
+    'track_index',
+    'track_asin',
+    'track_title',
+    'track_artist',
+    'track_artist_asin',
+    'track_artist_url',
+    'track_album',
+    'track_album_asin',
+    'track_album_url',
+    'track_album_track_url',
+    'track_playlist_track_url',
+    'track_sample_url',
+  ])
+
+  for track in embed_playlist.tracks:
+    writer.writerow([
+      playlist_asin,
+      get_playlist_url(playlist_asin=playlist_asin),
+      track.index,
+      track.asin,
+      track.title,
+      track.artist_name,
+      track.artist_asin,
+      get_artist_url(artist_asin=track.artist_asin),
+      track.album_name,
+      track.album_asin,
+      get_album_url(album_asin=track.album_asin),
+      get_album_track_url(album_asin=track.album_asin, track_asin=track.asin),
+      get_playlist_track_url(playlist_asin=playlist_asin, track_asin=track.asin),
+      track.sample_url,
+    ])
+
+  output_text = sio.getvalue()
+  print(output_text)
+
+
+def command_selenium_playlist_json(args):
+  selenium_url = args.selenium_url
+  playlist_asin = args.playlist_asin
+
+  timestamp_utc_aware = datetime.now(timezone.utc)
+
+  selenium_playlist = fetch_selenium_playlist(
+    selenium_url=selenium_url,
+    playlist_asin=playlist_asin,
+  )
+
+  print(
+    json.dumps(
+      {
+        'musics': selenium_playlist.tracks,
+        'fetched_at': timestamp_utc_aware.isoformat(),
+      },
+      ensure_ascii=False,
+    )
+  )
+
+
+def main():
+  import argparse
+  parser = argparse.ArgumentParser()
+  subparsers = parser.add_subparsers()
+
+  parser_embed_playlist_csv = subparsers.add_parser('embed_playlist_csv')
+  parser_embed_playlist_csv.add_argument('--playlist_asin', type=str, required=True)
+  parser_embed_playlist_csv.set_defaults(handler=command_embed_playlist_csv)
+
+  parser_selenium_playlist_json = subparsers.add_parser('selenium_playlist_json')
+  parser_selenium_playlist_json.add_argument('--selenium_url', type=str, required=True)
+  parser_selenium_playlist_json.add_argument('--playlist_asin', type=str, required=True)
+  parser_selenium_playlist_json.set_defaults(handler=command_selenium_playlist_json)
+
+  args = parser.parse_args()
+  if hasattr(args, 'handler'):
+    args.handler(args)
+  else:
+    parser.print_help()
+
+if __name__ == '__main__':
+  main()
